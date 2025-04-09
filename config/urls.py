a4351a9ff56153de90dafe969899916ff40223f5
Shohrukh Kholboev <kholboevshohrukh@gmail.com>
2025-04-09 16:58:07 +0500

    Change home page

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('users/', include('auth_users.urls')),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', lambda request: redirect('home')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
